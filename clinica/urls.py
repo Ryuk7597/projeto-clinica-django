5186a6c051f332a57ec0098667778e07a9871981
Natanael <ns3856600@gmail.com>
2025-07-10 17:11:12 -0400

    listagem de medicos para agendamento

--- clinica/urls.py
+++ clinica/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='clinica/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.dashboard_paciente, name='dashboard_paciente'),
+    path('agendamento/', views.listar_medicos, name='listar_medicos'),
+    
 ]
 
--- clinica/views.py
+++ clinica/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PacienteCreationForm
 from django.contrib.auth import logout
+from .models import Medico
 
 # Create your views here.
 def home(request):
@@ -22,3 +23,10 @@ def paciente_cadastro(request):
 def dashboard_paciente(request):
     # Por enquanto, apenas renderiza o template
     return render(request, 'clinica/dashboard_paciente.html')
+
+@login_required
+def listar_medicos(request):
+    # Busca todos os objetos Medico no banco de dados
+    medicos = Medico.objects.all()
+    # Envia a lista de medicos para o template
+    return render(request, 'clinica/listar_medicos.html', {'medicos': medicos})
